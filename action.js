e0304daf3d0242011b07aa28be0b91ab872a33c4
Luca Carducci <14071215+lucarducci@users.noreply.github.com>
2020-11-13 18:56:59 +0100

    build dist

--- action.js
+++ action.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 const execute = async function (opt) {
   console.log(opt);
-  
+
   // Set credentials
   if (!fs.existsSync("./AWSManagerConfig.json")) {
     let config = {
@@ -24,50 +24,57 @@ const execute = async function (opt) {
 
   // Get envs
   const envResponse = await eb.describeEnvironments().promise();
+  let envFound;
 
   for (let env of envResponse.Environments) {
+    let green = false;
+    let envType = opt.envTarget ? false : true;
     if (env.ApplicationName == opt.appName) {
       console.log("Checking environment ", env.EnvironmentName);
-      // Get tags
-      let tags = await eb.listTagsForResource({ ResourceArn: env.EnvironmentArn }).promise();
+      try {
+        // Get tags
+        let tags = await eb.listTagsForResource({ ResourceArn: env.EnvironmentArn }).promise();
 
-      // Check tags
-      let green = false;
-      let envType = opt.envTarget ? false : true;
-      for (let tag of tags.ResourceTags) {
-        if (tag.Key == opt.tagKeyColor && tag.Value == "green") {
-          console.log("Is green");
-          green = true;
+        // Check tags
+        for (let tag of tags.ResourceTags) {
+          if (tag.Key == opt.tagKeyColor && tag.Value == "green") {
+            console.log("Is green");
+            green = true;
+          }
+          if (tag.Key == opt.tagKeyEnv && tag.Value == opt.envTarget) {
+            console.log("Is env " + opt.envTarget);
+            envType = true;
+          }
         }
-        if (tag.Key == opt.tagKeyEnv && tag.Value == opt.envTarget) {
-          console.log("Is env " + opt.envTarget);
-          envType = true;
-        }
-      }
+      } catch (e) {}
     }
 
     // Execute
     if (green && envType) {
-      console.log("Execute ", env);
-      core.setOutput("source-env-name", env.EnvironmentName);
-      const destEnvName = env.EnvironmentName + "-" + opt.envTarget + "-" + new Date().getTime();
-      console.log("Dest env: " + destEnvName);
-      core.setOutput("dest-env-name", destEnvName);
-      // Clone env
-
-      // Update new environment
-      // Test healthCheck
-      // Swap URLs
-      // Update tags
-      // Delete old environment
-    } else {
-      console.log(
-        "No env found with tags: " +
-          opt.tagKeyColor +
-          " = 'green'" +
-          (opt.envTarget ? " and " + opt.tagKeyEnv + " = " + opt.envTarget : "")
-      );
+      envFound = env;
     }
+  }
+
+  if (envFound) {
+    console.log("Execute ", envFound);
+    core.setOutput("source-env-name", envFound.EnvironmentName);
+    const destEnvName = envFound.EnvironmentName + "-" + opt.envTarget + "-" + new Date().getTime();
+    console.log("Dest env: " + destEnvName);
+    core.setOutput("dest-env-name", destEnvName);
+    // Clone env
+
+    // Update new environment
+    // Test healthCheck
+    // Swap URLs
+    // Update tags
+    // Delete old environment
+  } else {
+    console.log(
+      "No env found with tags: " +
+        opt.tagKeyColor +
+        " = 'green'" +
+        (opt.envTarget ? " and " + opt.tagKeyEnv + " = " + opt.envTarget : "")
+    );
   }
 };
 
