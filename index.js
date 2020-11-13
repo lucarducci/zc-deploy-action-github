3a8358a4cfb5e3157c90ff6e4d578b536da73d95
Luca Carducci <14071215+lucarducci@users.noreply.github.com>
2020-11-13 14:49:51 +0100

    find env

--- index.js
+++ index.js
@@ -8,29 +8,52 @@ AWS.config.loadFromPath("./AWSManagerConfig.json");
 
 // Properties
 const appName = "zc-deploy-test";
-const tagKey = "type";
+const tagKeyColor = "color";
+const tagKeyEvn = "env";
+const envTarget = "stage";
 
 const execute = async function () {
   const eb = new AWS.ElasticBeanstalk({ region: "us-east-1" });
 
   const envResponse = await eb.describeEnvironments().promise();
 
-  for(let env of envResponse.Environments) {
+  for (let env of envResponse.Environments) {
     if (env.ApplicationName == appName) {
-        console.log("Checking environment ", env.EnvironmentName);
-        // Find green tag
-        let tags = await eb.listTagsForResource({ResourceArn: env.EnvironmentArn}).promise();
-        for(let tag of tags.ResourceTags){
-            if (tag.Key==tagKey && tag.Value =="green") {
-                console.log("Is green", env);
-                // Clone env
-                // Update new environment
-                // Test healthCheck
-                // Swap URLs
-                // Update tags
-                // Delete old environment 
-            }
-        } 
+      console.log("Checking environment ", env.EnvironmentName);
+      // Check tags
+      let tags = await eb.listTagsForResource({ ResourceArn: env.EnvironmentArn }).promise();
+      let green = false;
+      let envType = envTarget ? false : true;
+      for (let tag of tags.ResourceTags) {
+        if (tag.Key == tagKeyColor && tag.Value == "green") {
+          console.log("Is green");
+          green = true;
+        }
+        if (tag.Key == tagKeyEvn && tag.Value == envTarget) {
+          console.log("Is env " + envTarget);
+          envType = true;
+        }
+      }
+
+      // Execute
+      if (green && envType) {
+        console.log("Execute ", env);
+        core.setOutput("sourceEnvName", env.EnvironmentName);
+        const destEnvName = env.EnvironmentName + "_" + envTarget + "_" + new Date().getTime();
+        console.log("Dest env: " + destEnvName);
+        core.setOutput("destEnvName", destEnvName);
+        // Clone env
+
+        // Update new environment
+        // Test healthCheck
+        // Swap URLs
+        // Update tags
+        // Delete old environment
+      } else {
+        console.log(
+          "No env found with tags: " + tagKeyColor + " = 'green'" + (envTarget ? " and " + tagKeyEvn + " = " + envTarget : "")
+        );
+      }
     }
   }
 };
